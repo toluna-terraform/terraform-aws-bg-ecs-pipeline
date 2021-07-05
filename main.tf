e7a48f6a7d1e39981b823daea5d129ff5f07d15c
Raanan Gavrielli <raanan.gavrielli@toluna.com>
2021-07-05 14:25:38 +0300

    refrence to registry modules

--- main.tf
+++ main.tf
@@ -1,48 +1,48 @@
-locals{
-    source_repository_url = "https://bitbucket.org/${var.source_repository}"  
-    //TODO: handle image tag - replace "latest" - should be a build parameter and latest by default 
-    image_uri             = "${var.ecr_repo_url}:latest"
+locals {
+  source_repository_url = "https://bitbucket.org/${var.source_repository}"
+  //TODO: handle image tag - replace "latest" - should be a build parameter and latest by default 
+  image_uri = "${var.ecr_repo_url}:latest"
 }
 
 
 module "code-pipeline" {
-  #source                   = "toluna-terraform/code-pipeline/aws"
-  #version                  = "~>0.0.5"
-  source                   = "../terraform-aws-code-pipeline"
+  source  = "toluna-terraform/code-pipeline/aws"
+  version = "~>0.0.5"
+  #source                   = "../terraform-aws-code-pipeline"
   env_name                 = var.env_name
   source_repository        = var.source_repository
-  s3_bucket                = aws_s3_bucket.codepipeline_bucket.bucket 
-  code_build_projects      = [module.code-build.attributes.name] 
-  code_deploy_applications = [module.code-deploy.attributes.name] 
+  s3_bucket                = aws_s3_bucket.codepipeline_bucket.bucket
+  code_build_projects      = [module.code-build.attributes.name]
+  code_deploy_applications = [module.code-deploy.attributes.name]
   trigger_branch           = var.trigger_branch
-  trigger_events           = ["push","merge"]
+  trigger_events           = ["push", "merge"]
   depends_on = [
-    aws_s3_bucket.codepipeline_bucket, 
+    aws_s3_bucket.codepipeline_bucket,
   ]
 }
 
 module "code-build" {
-  #source                               = "toluna-terraform/code-build/aws"
-  #version                              = "~>0.0.5"
-  source                                = "../terraform-aws-code-build"
+  source  = "toluna-terraform/code-build/aws"
+  version = "~>0.0.5"
+  #source                                = "../terraform-aws-code-build"
   env_name                              = var.env_name
-  s3_bucket                             = aws_s3_bucket.codepipeline_bucket.bucket 
+  s3_bucket                             = aws_s3_bucket.codepipeline_bucket.bucket
   privileged_mode                       = true
   environment_variables_parameter_store = var.environment_variables_parameter_store
-  environment_variables                 = merge(var.environment_variables, {APPSPEC = templatefile("${path.module}/templates/appspec.json.tpl",{ yoyo = "yo"}) }) //TODO: try to replace with file
-  buildspec_file                        = templatefile("${path.module}/templates/buildspec.yml.tpl", { IMAGE_URI = local.image_uri, DOCKERFILE_PATH = var.dockerfile_path, IMAGE_REPO_NAME = var.ecr_repo_name ,ADO_USER = data.aws_ssm_parameter.ado_user.value ,ADO_PASSWORD = data.aws_ssm_parameter.ado_password.value})
+  environment_variables                 = merge(var.environment_variables, { APPSPEC = templatefile("${path.module}/templates/appspec.json.tpl", { yoyo = "yo" }) }) //TODO: try to replace with file
+  buildspec_file                        = templatefile("${path.module}/templates/buildspec.yml.tpl", { IMAGE_URI = local.image_uri, DOCKERFILE_PATH = var.dockerfile_path, IMAGE_REPO_NAME = var.ecr_repo_name, ADO_USER = data.aws_ssm_parameter.ado_user.value, ADO_PASSWORD = data.aws_ssm_parameter.ado_password.value })
   depends_on = [
-    aws_s3_bucket.codepipeline_bucket, 
+    aws_s3_bucket.codepipeline_bucket,
   ]
 }
 
 
 module "code-deploy" {
-  #source             = "toluna-terraform/code-deploy/aws"
-  #version            = "~>0.0.1"
-  source                   = "../terraform-aws-code-deploy"
+  source  = "toluna-terraform/code-deploy/aws"
+  version = "~>0.0.1"
+  #source                   = "../terraform-aws-code-deploy"
   env_name           = var.env_name
-  s3_bucket          = aws_s3_bucket.codepipeline_bucket.bucket 
+  s3_bucket          = aws_s3_bucket.codepipeline_bucket.bucket
   ecs_service_name   = var.ecs_service_name
   ecs_cluster_name   = var.ecs_cluster_name
   alb_listener_arn   = var.alb_listener_arn
@@ -53,10 +53,10 @@ module "code-deploy" {
 
 
 resource "aws_s3_bucket" "codepipeline_bucket" {
- bucket = "s3-${var.env_name}-codepipeline"
- acl = "private"
- tags = tomap({
-   UseWithCodeDeploy = true
-   created_by        = "terraform"
- })
+  bucket = "s3-${var.env_name}-codepipeline"
+  acl    = "private"
+  tags = tomap({
+    UseWithCodeDeploy = true
+    created_by        = "terraform"
+  })
 }
